f0bfa79b4809309b274ecae149cd5758d7f8ea8a
Alex Shyba <alex@altola.co>
2018-11-23 09:37:32 -0800

    completing sport skill predicate

--- fitness/server/Sitecore.HabitatHome.Fitness.Segmentation/Predicates/SportsSkillPredicate.cs
+++ fitness/server/Sitecore.HabitatHome.Fitness.Segmentation/Predicates/SportsSkillPredicate.cs
@@ -11,21 +11,21 @@ namespace Sitecore.HabitatHome.Fitness.Segmentation.Predicates
     public class SportsSkillPredicate : ICondition, IContactSearchQueryFactory
     {
         public string SportName { get; set; }
+
         public int SkillLevel { get; set; }
+
         public NumericOperationType Comparison { get; set; }
 
         public bool Evaluate(IRuleExecutionContext context)
         {
-            var ratings = context.Fact<Contact>().GetFacet<SportsFacet>(SportsFacet.DefaultKey).Ratings;
-            return ratings.Keys.Any(key => key == SportName) && Comparison.Evaluate(ratings[SportName], SkillLevel);
+            return context.Fact<Contact>().GetFacet<SportsFacet>(SportsFacet.DefaultKey).Ratings.Any(r => r.Key == SportName && Comparison.Evaluate(r.Value, SkillLevel));
         }
 
         public Expression<Func<Contact, bool>> CreateContactSearchQuery(IContactSearchQueryContext context)
         {
             // This is duplicating the Evaluate method on purpose
             // Do not modify this expression, segmentation engine doesn't like it
-            return contact => contact.GetFacet<SportsFacet>(SportsFacet.DefaultKey).Ratings.Keys.Any(key => key == SportName) &&
-                               Comparison.Evaluate(contact.GetFacet<SportsFacet>(SportsFacet.DefaultKey).Ratings[SportName], SkillLevel);
+            return contact => contact.GetFacet<SportsFacet>(SportsFacet.DefaultKey).Ratings.Any(r => r.Key == SportName && Comparison.Evaluate(r.Value, SkillLevel));
         }
     }
 }
