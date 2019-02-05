abe446a4e6a84987c72401a6767a1b3b3f66a8e0
Alex Shyba <alex@altola.co>
2019-02-04 18:29:20 -0800

    new automation plan, retiring email service; exm email, minor cleanup
    (cherry picked from commit 1d9aeaeb47595c7818172100c7a6213de544544c)

--- fitness/server/Fitness.Collection/Services/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sitecore.HabitatHome.Fitness.Collection.Services
-{
-    public interface IEmailService
-    {
-        bool SendAppInviteEmail(string hostName, string email, string name, string alias);
-    }
-}
