7b1001d3853fa675d5cc5de66f2dc255990e69d9
Jeff L'Heureux <jel@sitecore.net>
2019-08-21 11:36:03 -0400

    Update AssemblyInfo files

--- fitness/server/src/Foundation/Analytics/code/Properties/AssemblyInfo.cs
+++ fitness/server/src/Foundation/Analytics/code/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.HabitatHome.Fitness.Foundation.Collection.Model")]
+[assembly: AssemblyTitle("Sitecore.HabitatHome.Fitness.Foundation.Analytics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sitecore.HabitatHome.Fitness.Foundation.Collection.Model")]
+[assembly: AssemblyProduct("Sitecore.HabitatHome.Fitness.Foundation.Analytics")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
