5f1e10bf5cdc55746261beeb5f6d21838d049a6c
Jeff L'Heureux <jel@sitecore.net>
2019-08-21 17:22:37 -0400

    Fix AssemblyInfo typo

--- fitness/server/src/Feature/Automation/code/Properties/AssemblyInfo.cs
+++ fitness/server/src/Feature/Automation/code/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.HabitatHome.Fitness.Featue.Automation")]
+[assembly: AssemblyTitle("Sitecore.HabitatHome.Fitness.Feature.Automation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
