e8dc68a08962e834183afcf1878e28fa74d66eb5
Scott Mulligan <scm@sitecore.net>
2021-04-26 15:32:28 -0400

    Fixing a few issues by switching to BOXEVER_APIURL

--- docker/images/demo-boxever/src/Controllers/BoxeverController.cs
+++ docker/images/demo-boxever/src/Controllers/BoxeverController.cs
@@ -31,7 +31,7 @@ namespace Sitecore.Integrations.Boxever.Controllers
         {
             this.configuration = configuration;
 
-            apiUrl = Environment.GetEnvironmentVariable("BOXEVER_PROXY_URL"); 
+            apiUrl = Environment.GetEnvironmentVariable("BOXEVER_APIURL"); 
             clientKey = Environment.GetEnvironmentVariable("BOXEVER_CLIENTKEY"); 
             apiToken = Environment.GetEnvironmentVariable("BOXEVER_APITOKEN");
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
